4799f4aced8d9d6dae2ed91ad68c259ec0137c8c
mojahige <mojahige@gmail.com>
2022-11-18 21:10:25 +0900

    Change default port

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_ROOT = "./";
-export const DEFAULT_PORT = 8000;
+export const DEFAULT_PORT = 8080;
 export const ROOT_ARGUMENTS = ["-r", "--root"] as const;
 export const PORT_ARGUMENTS = ["-p", "--port"] as const;
