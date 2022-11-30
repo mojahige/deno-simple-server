a7a57a27404e233a862f7abac3d14f63fb39cd44
mojahige <mojahige@gmail.com>
2022-11-30 21:36:51 +0900

    Update deps
    Changed export method so that all Deno assertions can be used.
    Removed unused "sinon".

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,4 @@
 export { serve } from "https://deno.land/std@0.165.0/http/server.ts";
 export { serveDir } from "https://deno.land/std@0.165.0/http/file_server.ts";
-export { assertObjectMatch } from "https://deno.land/std@0.165.0/testing/asserts.ts";
+export * as asserts from "https://deno.land/std@0.165.0/testing/asserts.ts";
 export { parse } from "https://deno.land/std@0.165.0/flags/mod.ts";
-export { default as sinon } from "https://esm.sh/v98/*sinon@14.0.2";
