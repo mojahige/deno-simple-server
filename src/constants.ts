36b15747909e5a1ab2dda0769088baad6532272d
mojahige <13248811+mojahige@users.noreply.github.com>
2023-02-08 12:32:03 +0900

    test

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const DEFAULT_ROOT = "./";
 export const DEFAULT_PORT = 8080;
 export const ROOT_ARGUMENTS = ["-r", "--root"] as const;
 export const PORT_ARGUMENTS = ["-p", "--port"] as const;
+export const FOO = 'foo'
